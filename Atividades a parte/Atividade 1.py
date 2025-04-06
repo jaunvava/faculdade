3316e0158add9e634b6db5753eba9a48b8133b5d
jaunvava <mortalteck1020@gmail.com>
2025-04-06 11:03:12 -0300

    feat: organização das pastas

---
+++ Atividades a parte/Atividade 1.py
@@ -0,0 +1,7 @@
+idade = int(print('digite sua idade: '))
+
+
+
+# palavras = ['gato', 'janela', 'defenestrar']
+# for p in palavras:
+#     print(p, len(p))
---
+++ README.md
@@ -0,0 +1,4 @@
+# Apostila 1
+- Nessa apostila vemos sintax básica do python e estrutura de descição, IF e Else e Elif
+
+# Apostila 2
